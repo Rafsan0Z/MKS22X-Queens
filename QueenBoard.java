a79411301f229473b71a90ddc55c6d17ff34e4a2
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-02-07 09:38:04 -0500

    addQueen method updated to mark horizontally and vertically

--- QueenBoard.java
+++ QueenBoard.java
@@ -11,6 +11,13 @@ public class QueenBoard{
       System.out.println("Queen already exists!");
       return false;
     }
+    int[r][c] = -1;
+    for(int i = 0; i < size && i != c; i++){
+      int[r][i] += 1;
+    }
+    for(int j = 0; j < size && j != r; j++){
+      int[j][c] += 1;
+    }
   }
 
   private boolean removeQueen(int r, int c){
