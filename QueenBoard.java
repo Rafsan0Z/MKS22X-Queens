956f91a65dbe2925fd60e21882a2882aa9a4fb73
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-02-25 19:44:01 -0500

    runProcess and printLines method imported from JournalDev to QueenBoard!

--- QueenBoard.java
+++ QueenBoard.java
@@ -144,6 +144,23 @@ public class QueenBoard{
   return result;
   }
 
+  private static void printLines(String cmd, InputStream ins) throws Exception {
+        String line = null;
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(ins));
+        while ((line = in.readLine()) != null) {
+            System.out.println(cmd + " " + line);
+        }
+      }
+
+  private static void runProcess(String command) throws Exception {
+        Process pro = Runtime.getRuntime().exec(command);
+        printLines(command + " stdout:", pro.getInputStream());
+        printLines(command + " stderr:", pro.getErrorStream());
+        pro.waitFor();
+        System.out.println(command + " exitValue() " + pro.exitValue());
+      }
+
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     System.out.println("If you wish to run a driver file press &. Otherwise press %");
