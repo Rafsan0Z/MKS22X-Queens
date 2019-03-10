370f8e4276206790792f425825af1edc9c5d40a2
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-10 13:41:09 -0400

    Mr. K test passed!

--- QueenBoard.java
+++ QueenBoard.java
@@ -139,13 +139,31 @@ public class QueenBoard{
   return result;
   }
 
+  public static void runTest(int i){
+  QueenBoard b;
+  int[]tests =   {1,2,3,4,5,8};
+  int[]answers = {1,0,0,2,10,92};
+  if(i >= 0 && i < tests.length ){
+    int size = tests[i];
+    int correct = answers[i];
+    b = new QueenBoard(size);
+    int ans  = b.countSolutions();
+
+    if(correct==ans){
+      System.out.println("PASS board size: "+tests[i]+" "+ans);
+    }else{
+      System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+    }
+  }
+}
+
   public static void main(String[] args){
 
     QueenBoard q = new QueenBoard(Integer.parseInt(args[0]));
     q.solve();
     System.out.println(q);
     System.out.println(q.countSolutions());
-
+    runTest(1);
   }
 
 }
